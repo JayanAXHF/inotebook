f09ae507d1c25ac9336028bc3fbbe178a1286e50
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2021-09-02 17:32:43 +0530

    added some routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("express loaded successfully");
 });
+app.get("/api.v1.login", (req, res) => {
+  res.send("login loaded successfully");
+});
+app.get("/api.v1.signup", (req, res) => {
+  res.send("signup loaded successfully");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
