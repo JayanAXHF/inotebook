47bb7f90962a541c873d1b97b193e135a64fcf65
Jayan <sunil.chdry@gmail.com>
2021-09-13 11:06:09 +0530

    Done (nothing is final

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,13 @@ const app = express();
 const port = 5000;
 var cors = require("cors");
 
-<<<<<<< HEAD
 app.use(cors());
 app.use(express.json());
 
 //*Available routes
 app.use("/api/auth", require("./routes/auth.js"));
 app.use("/api/notes", require("./routes/notes.js"));
-=======
+
 app.get("/", (req, res) => {
   res.send("express loaded successfully");
 });
@@ -22,7 +21,6 @@ app.get("/api.v1.login", (req, res) => {
 app.get("/api.v1.signup", (req, res) => {
   res.send("signup loaded successfully");
 });
->>>>>>> f09ae507d1c25ac9336028bc3fbbe178a1286e50
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`);
